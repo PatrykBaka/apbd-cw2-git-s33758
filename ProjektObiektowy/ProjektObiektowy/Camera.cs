f89cb31c11006d3432ac838bb4af8aa4b056afc2
PatrykBaka <s33758@pjwstk.edu.pl>
2026-03-22 14:38:32 +0100

    Dodanie reszty klas dziedziczących po klasie abstract Sprzet

---
+++ ProjektObiektowy/ProjektObiektowy/Camera.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjektObiektowy;
+
+public class Camera : Sprzet
+{
+    
+    public int LiczbaKlatek { get; set; }
+    public string Rozdzielczosc  { get; set; }
+
+    public Camera(string nazwa, int liczbaKlatek, string rozdzielczosc) : base(nazwa)
+    {
+        LiczbaKlatek = liczbaKlatek;
+        Rozdzielczosc = rozdzielczosc;
+    }
+    
+}
---
+++ ProjektObiektowy/ProjektObiektowy/Projector.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjektObiektowy;
+
+public class Projector : Sprzet
+{
+    
+    public int Cale { get; set; }
+    public int Jakosc { get; set; }
+
+    public Projector(string nazwa, int cale, int jakosc) : base(nazwa)
+    {
+        Cale =  cale;
+        Jakosc =  jakosc;
+    }
+    
+}
