081231a8c206977b678c2afdbffcc09df6aedc25
PatrykBaka <s33758@pjwstk.edu.pl>
2026-03-23 11:05:31 +0100

    Stworzenie klasy Wypozyczenie w innym branchu

--- ProjektObiektowy/ProjektObiektowy/Wypozyczenie.cs
+++ ProjektObiektowy/ProjektObiektowy/Wypozyczenie.cs
@@ -3,6 +3,21 @@
 public class Wypozyczenie
 {
     
+    public Sprzet _Sprzet { get; set; }
+    public Osoba _Osoba { get; set; }
+    public DateTime DataWypozyczenia { get; set; }
+    public DateTime DataZwrotu { get; set; }
+    public DateTime? DataFaktycznegoZwrotu { get; set; }
     
-    
+    public decimal Kara { get; set; }
+
+    public Wypozyczenie(Sprzet sprzet, Osoba osoba, DateTime dataWypozyczenia, DateTime dataZwrotu, DateTime? dataFaktycznegoZwrotu)
+    {
+        _Sprzet = sprzet;
+        _Osoba = osoba;
+        DataWypozyczenia = dataWypozyczenia;
+        DataZwrotu = dataZwrotu;
+        DataFaktycznegoZwrotu = dataFaktycznegoZwrotu;
+        Kara = 0;
+    }
 }
